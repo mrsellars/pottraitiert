74000b368c772aaaea7b0585d6b9ca6283ef6663
Tim <tkeller.online@googlemail.com>
2013-01-06 19:51:33 +0100

    Add pony for email

--- pottraitiert.rb
+++ pottraitiert.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'pony'
 require 'rack/recaptcha'
 use Rack::Recaptcha, :public_key => ENV['RECAPTCHA_PUBLIC_KEY'], :private_key => ENV['RECAPTCHA_PRIVATE_KEY']
 #just using recaptcha_valid?
@@ -19,14 +20,28 @@ end
 post '/nachricht'do
  @notice = ''
  @errors = {}
- recaptcha_valid?
- @errors[:name]  = 'Bitte einen Namen angeben.' if params[:nachricht][:name].empty? 
  email = params[:nachricht][:email]
  @errors[:email] = 'Bitte eine richtige Email-Adresse angeben.' if (email.empty? || valid_email?(email).nil?)
  @errors[:text]  = 'Bitte einen Text angeben.' if params[:nachricht][:text].empty?
  @errors[:captcha] = 'Das eingegebene reCAPTCHA war falsch.' unless recaptcha_valid?
- 
+
  if @errors.empty?
+   Pony.mail({:from => params[:email],
+   	:to => 'tkeller.online@googlemail.com',
+   	:subject => "Nachricht von pottraitiert.de",
+   	:body => params[:nachricht][:email],
+   	:port => '587',
+   	:via => :smtp,
+   	:via_options => {
+   	  :address              => 'smtp.sendgrid.net',
+   	  :port                 => '587',
+   	  :enable_starttls_auto => true,
+   	  :user_name            => ENV['SENDGRID_USERNAME'],
+   	  :password             => ENV['SENDGRID_PASSWORD'],
+   	  :authentication       => :plain,
+   	  :domain               => 'heroku.com'
+   	})
+   })
    @notice = 'Deine Nachricht wurde verschickt :-).'
    params[:nachricht] = {}
  else
