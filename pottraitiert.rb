c0b776ae195c978100d5d70c3614026d27c54b6d
Tim <tkeller.online@googlemail.com>
2013-01-06 18:05:21 +0100

    Add recaptcha

--- pottraitiert.rb
+++ pottraitiert.rb
@@ -1,4 +1,8 @@
 require 'sinatra'
+require 'rack/recaptcha'
+use Rack::Recaptcha, :public_key => ENV['RECAPTCHA_PUBLIC_KEY'], :private_key => ENV['RECAPTCHA_PRIVATE_KEY']
+#just using recaptcha_valid?
+helpers Rack::Recaptcha::Helpers
 
 configure do
   set :public_folder, File.dirname(__FILE__) + '/_site'
@@ -15,20 +19,21 @@ end
 post '/nachricht'do
  @notice = ''
  @errors = {}
- 
+ recaptcha_valid?
  @errors[:name]  = 'Bitte einen Namen angeben.' if params[:nachricht][:name].empty? 
  email = params[:nachricht][:email]
  @errors[:email] = 'Bitte eine richtige Email-Adresse angeben.' if (email.empty? || valid_email?(email).nil?)
  @errors[:text]  = 'Bitte einen Text angeben.' if params[:nachricht][:text].empty?
- 
- #require 'ruby-debug'
- #debugger
+ @errors[:captcha] = 'Das eingegebene reCAPTCHA war falsch.' unless recaptcha_valid?
  
  if @errors.empty?
    @notice = 'Deine Nachricht wurde verschickt :-).'
+   params[:nachricht] = {}
  else
    @notice = 'Es ist ein Fehler aufgetreten :-(.'
  end
+ 
+ erb :kontakt
 end
 
 def valid_email? email
