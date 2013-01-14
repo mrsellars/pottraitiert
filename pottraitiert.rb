4689e853a28f085df63916368b3463b1045ec90c
Tim <tkeller.online@googlemail.com>
2013-01-15 00:48:13 +0100

    Add includes and javascript based active link menu

--- pottraitiert.rb
+++ pottraitiert.rb
@@ -21,25 +21,24 @@ post '/nachricht'do
  @notice = ''
  @errors = {}
  email = params[:nachricht][:email]
- @errors[:email] = 'Bitte eine richtige Email-Adresse angeben.' if (email.empty? || valid_email?(email).nil?)
+ #@errors[:email] = 'Bitte eine richtige Email-Adresse angeben.' #if (email.empty? || valid_email?(email).nil?)
  @errors[:text]  = 'Bitte einen Text angeben.' if params[:nachricht][:text].empty?
  @errors[:captcha] = 'Das eingegebene reCAPTCHA war falsch.' unless recaptcha_valid?
 
  if @errors.empty?
-   Pony.mail({:from => params[:email],
+   Pony.mail({:from => params[:nachricht][:email],
    	:to => 'tkeller.online@googlemail.com',
    	:subject => "Nachricht von pottraitiert.de",
-   	:body => params[:nachricht][:email],
+   	:body => params[:nachricht][:text],
    	:port => '587',
    	:via => :smtp,
    	:via_options => {
    	  :address              => 'smtp.sendgrid.net',
-   	  :port                 => '587',
-   	  :enable_starttls_auto => true,
+   	  :port                 =>  587,
    	  :user_name            => ENV['SENDGRID_USERNAME'],
    	  :password             => ENV['SENDGRID_PASSWORD'],
    	  :authentication       => :plain,
-   	  :domain               => 'heroku.com'
+   	  :enable_starttls_auto => true
    	}
    })
    @notice = 'Deine Nachricht wurde verschickt :-).'
